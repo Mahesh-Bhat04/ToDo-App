965488c5b89dc0c65def8ccd0bd69cf3f4e93143
Mahesh Bhat <bhatmahesh0401@gmail.com>
2024-06-25 18:29:00 +0530

    Initial commit

--- app/src/main/java/com/example/todo_app/ToDoListPage.kt
+++ app/src/main/java/com/example/todo_app/ToDoListPage.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -124,7 +123,6 @@ fun TodoItem(item: Todo, onDelete : () -> Unit) {
                 contentDescription = "delete",
                 tint = Color.White
             )
-
         }
     }
 }
